eec4876636161248aac851ebbe2e6e5e01a33df6
aaron-wallin <aaron.wallin@gmail.com>
2018-08-10 13:24:22 -0700

    Inflight entertainment

---
+++ AProblemTester/InflightEntertainmentTester.cs
@@ -0,0 +1,51 @@
+﻿using FindTwoIntegersThatDoNotExceedMax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AProblemTester
+{
+
+    [TestClass]
+    public class InflightEntertainmentTester
+    {
+        [TestMethod]
+        public void TestA()
+        {
+            var movieLengths = new int[] { 20, 10, 60, 11, 88, 52 };
+            var flightLength = 80;
+
+            var testObj = new TwoMaxCombined(flightLength, movieLengths);
+            var result = testObj.DetermineMatch();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestB()
+        {
+            var movieLengths = new int[] { 20, 10, 60, 11, 88, 52 };
+            var flightLength = 99;
+
+            var testObj = new TwoMaxCombined(flightLength, movieLengths);
+            var result = testObj.DetermineMatch();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestC()
+        {
+            var movieLengths = new int[] { 20, 10, 60, 11, 88, 52 };
+            var flightLength = 500;
+
+            var testObj = new TwoMaxCombined(flightLength, movieLengths);
+            var result = testObj.DetermineMatch();
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ FindTwoIntegersThatDoNotExceedMax/TwoMaxCombined.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTwoIntegersThatDoNotExceedMax
+{
+    public class TwoMaxCombined
+    {
+        private readonly int _flightLength;
+        private readonly int[] _listOfMovieLengths;
+
+        public TwoMaxCombined(int flightLength, int[] listOfMovieLengths)
+        {
+            this._flightLength = flightLength;
+            this._listOfMovieLengths = listOfMovieLengths;
+        }
+
+        public bool DetermineMatch()
+        {
+            bool result = false;
+            var set = new HashSet<int>();            
+
+            for(int i = 0; i < _listOfMovieLengths.Length; i++)
+            {
+                int firstMovieLength = _listOfMovieLengths[i];
+                int secondTargetMovieLength = _flightLength - firstMovieLength;
+                
+                if (set.Contains(secondTargetMovieLength))
+                {
+                    return true;
+                }
+
+                set.Add(firstMovieLength);
+            }
+
+            return result;
+        }
+    }
+}
